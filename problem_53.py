4dfd8c98944d2b84e0ca0bc7eb4c9217a67f1037
Akhil-01-pra <akhildhanabalan@gmail.com>
2026-01-30 15:43:31 +0530

    change53

--- problem_53.py
+++ problem_53.py
@@ -2,8 +2,5 @@
 # Find and fix the error
 
 def are_anagrams(str1, str2):
-    return sorted(str1) == sorted(str2)
-
-word1 = "Listen"
-word2 = "Silent"
+    return sorted(str1) == sorted(str2) 
 print(f"Are anagrams: {are_anagrams(word1, word2)}")
