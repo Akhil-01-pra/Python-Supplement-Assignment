0b2a80fc30fd33290addcca176eb9b3d9b677459
Akhil-01-pra <akhildhanabalan@gmail.com>
2026-01-30 15:44:39 +0530

    change55

--- problem_55.py
+++ problem_55.py
@@ -2,13 +2,11 @@
 # Find and fix the error
 
 def count_frequency(lst):
-    freq = {}
+    frequency = {}
     for item in lst:
-        if item in freq:
-            freq[item] += 1
+        if item in frequency:
+            frequency[item] += 1
         else:
-            freq[item] = 1
-    return freq
-
-numbers = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+            frequency[item] = 1
+    return frequency
 print(f"Frequency: {count_frequency(numbers)}")
