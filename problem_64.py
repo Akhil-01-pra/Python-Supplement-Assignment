de525d6aa17b0261b175ab47640ab4f5382de8f7
Akhil-01-pra <akhildhanabalan@gmail.com>
2026-01-30 15:53:09 +0530

    change64

--- problem_64.py
+++ problem_64.py
@@ -11,10 +11,11 @@ def merge_sorted(list1, list2):
         else:
             merged.append(list2[j])
             j += 1
-    merged.extend(list1[i:])
-    merged.extend(list2[j:])
-    return merged
-
-l1 = [1, 3, 5]
-l2 = [2, 4, 6]
+    while i < len(list1):
+        merged.append(list1[i])
+        i += 1
+    while j < len(list2):
+        merged.append(list2[j])
+        j += 1
+    return merged       
 print(f"Merged: {merge_sorted(l1, l2)}")
