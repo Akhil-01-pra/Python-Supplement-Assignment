7a7d86cb7b4d4a18a1a28f517cf6221fdeb716a2
Akhil-01-pra <akhildhanabalan@gmail.com>
2026-01-30 16:14:14 +0530

    change97

--- problem_97.py
+++ problem_97.py
@@ -2,14 +2,12 @@
 # Find and fix the error
 
 def remove_element(nums, val):
-    i = 0
-    for j in range(len(nums)):
-        if nums[j] != val:
-            nums[i] = nums[j]
-            i += 1
-    return i
-
-numbers = [3, 2, 2, 3, 4, 5]
-length = remove_element(numbers, 3)
+    length = 0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[length] = nums[i]
+            length += 1
+    return length
+    
 print(f"New length: {length}")
 print(f"Modified list: {numbers[:length]}")
