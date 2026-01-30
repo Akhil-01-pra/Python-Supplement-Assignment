c946cebd6405e03c21de51172f387bc22b72af56
Akhil-01-pra <akhildhanabalan@gmail.com>
2026-01-30 16:08:28 +0530

    change84

--- problem_84.py
+++ problem_84.py
@@ -2,10 +2,5 @@
 # Find and fix the error
 
 def contains_substring(text, substr):
-    for i in range(len(text) - len(substr)):
-        if text[i:i+len(substr)] == substr:
-            return True
-    return False
-
-sentence = "Python programming is fun"
+    return substr in text   
 print(f"Contains 'fun': {contains_substring(sentence, 'fun')}")
