de0af0ba034d310d6e83311b8fadb8b7b09886e7
Akhil-01-pra <akhildhanabalan@gmail.com>
2026-01-30 15:59:42 +0530

    change73

--- problem_73.py
+++ problem_73.py
@@ -1,17 +1,15 @@
 # Problem 73: Find maximum difference between elements
 # Find and fix the error
 
-def max_difference(arr):
-    if len(arr) < 2:
+def max_difference(lst):                
+    if len(lst) < 2:
         return 0
-    min_val = arr[0]
-    max_diff = 0
-    for i in range(1, len(arr)):
-        if arr[i] - min_val > max_diff:
-            max_diff = arr[i] - min_val
-        if arr[i] < min_val:
-            min_val = arr[i]
+    min_elem = lst[0]
+    max_diff = lst[1] - lst[0]
+    for i in range(1, len(lst)):
+        if lst[i] - min_elem > max_diff:
+            max_diff = lst[i] - min_elem
+        if lst[i] < min_elem:
+            min_elem = lst[i]
     return max_diff
-
-numbers = [7, 1, 5, 3, 6, 4]
 print(f"Max difference: {max_difference(numbers)}")
