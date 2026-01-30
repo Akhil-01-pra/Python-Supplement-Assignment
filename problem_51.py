82b1722369ce9fadf85753f0f07966550e2b0b0a
Akhil-01-pra <akhildhanabalan@gmail.com>
2026-01-30 15:42:17 +0530

    change51

--- problem_51.py
+++ problem_51.py
@@ -3,10 +3,6 @@
 
 def reverse_words(sentence):
     words = sentence.split()
-    reversed_words = []
-    for word in words:
-        reversed_words.append(word[::-1])
-    return " ".join(reversed_words)
-
-text = "Hello World"
+    reversed_words = ' '.join(reversed(words))
+    return reversed_words       
 print(f"Reversed words: {reverse_words(text)}")
