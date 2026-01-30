d03b1d6c28c4cb7a765adbb9dbcd9586b7c862df
Akhil-01-pra <akhildhanabalan@gmail.com>
2026-01-30 15:44:02 +0530

    change54

--- problem_54.py
+++ problem_54.py
@@ -2,11 +2,9 @@
 # Find and fix the error
 
 def nth_fibonacci(n):
-    if n <= 1:
-        return n
     a, b = 0, 1
-    for i in range(2, n):
+    for _ in range(n):
         a, b = b, a + b
-    return b
+    return a    
 
 print(f"10th Fibonacci number: {nth_fibonacci(10)}")
