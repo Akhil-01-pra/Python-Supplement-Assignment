4e0e14d80f32cda948292727a69c51c859c9c381
Akhil-01-pra <akhildhanabalan@gmail.com>
2026-01-30 16:13:46 +0530

    change96

--- problem_96.py
+++ problem_96.py
@@ -2,13 +2,11 @@
 # Find and fix the error
 
 def two_sum(nums, target):
-    seen = {}
-    for i in range(len(nums)):
-        complement = target - nums[i]
-        if complement in seen:
-            return [seen[complement], i]
-        seen[nums[i]] = i
-    return []
-
-numbers = [2, 7, 11, 15]
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return (num_map[complement], i)
+        num_map[num] = i
+    return None
 print(f"Indices: {two_sum(numbers, 9)}")
